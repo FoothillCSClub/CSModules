ae72040eaa2ec5c7cd15726f4cf820ad5ec4f672
joshuaptfan <joshuaptfan@gmail.com>
2017-05-02 09:50:32 -0700

    Hamburger menu prototype

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-function toggleMenu() {
-    var menu = document.getElementById('nav-menu');
-    if (menu.style.display === 'block')
-        menu.style.display = 'none';
-    else
-        menu.style.display = 'block';
+function toggleNav() {
+	var list = document.getElementById('nav-list');
+	if (list.style.display === 'block')
+		list.style.display = 'none';
+	else
+		list.style.display = 'block';
 }
 
 function setFrame(course, chapter, section) {
-    document.getElementById('frame').src = 'https://fgamedia.org/faculty/anand/cs'.concat(course, '/modules/cs_', course.toUpperCase(), '_', chapter, '_', section, '.html');
+	document.getElementById('frame').src = 'https://fgamedia.org/faculty/anand/cs'.concat(course, '/modules/cs_', course.toUpperCase(), '_', chapter, '_', section, '.html');
 }
