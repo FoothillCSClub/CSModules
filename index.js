e63174e1201f440f75141fc294656d032e1332e3
joshuaptfan <joshuaptfan@gmail.com>
2017-05-03 09:08:38 -0700

    Add all modules in all available courses

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
+var currCourse, currChapter, currSection;
+
 window.onreadystatechange = initList();
 
 function initList() {
 	var list,
 	    navList = document.getElementById('nav-list'),
 	    navCourses = document.getElementById('nav-courses');
-	var navList = document.getElementById('nav-list'),
-	    navCourses = document.getElementById('nav-courses');
 	list = {
+		"1a":{
+			"c":["1a","1b","2a","2b","3a","3b","4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b","10c"],
+			"s":[5,2,7,7,4,5,5,6,6,5,5,6,8,5,6,6,2,3,7,4,8]
+		},
+		"1b":{
+			"c":["1a","1b","2a","2b","3a","3b","4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b","10c"],
+			"s":[7,7,6,5,1,5,5,4,8,8,6,5,3,4,5,8,5,6,4,6,6]
+		},
+		"1c":{
+			"c":["1a","1b","2a","2b","3a","3b","4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b","11a","11b"],
+			"s":[8,9,4,7,5,5,4,5,6,6,3,4,4,2,4,3,4,5,6,3,5,2]
+		},
+		"2a":{
+			"c":["1a","1b","2a","2b","3a","3b","3c","4a","4b","4c","5a","5b","6a","6b","6c","7a","7b","8a","8b","9a","9b","9c","10a","10b"],
+			"s":[5,7,7,5,8,6,5,6,4,6,6,5,5,5,6,8,5,5,5,2,3,3,4,4]
+		},
+		"2b":{
+			"c":["1a","1b","2a","2b","3a","3b","4a","4b","4c","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b","10c"],
+			"s":[8,7,7,7,8,7,5,3,8,6,5,4,4,5,6,5,4,6,4,5,1,4]
+		},
 		"2c":{
 			"c":["1a","1b","2a","2b","3a","3b","4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b","11a","11b"],
 			"s":[9,9,4,7,5,5,4,5,6,6,3,4,4,2,4,4,6,4,6,3,5,2]
@@ -26,13 +46,21 @@ function initList() {
 		courseBtn.setAttribute('onclick', 'setCourse("'.concat(course, '")'));
 		courseBtn.innerHTML = course.toUpperCase();
 		navCourses.appendChild(courseBtn);
-		// Generate course module container
+		// Generate course module list container
 		var navModules = document.createElement('div');
 		navModules.id = course;
 		navList.appendChild(navModules);
 		// Generate module list
+		// TEMP: Generate course title
+		navModules.appendChild(document.createElement('br'));
+		var courseSpan = document.createElement('span');
+		courseSpan.style.fontSize = '250%';
+		courseSpan.innerHTML = course.toUpperCase();
+		navModules.appendChild(courseSpan);
+		navModules.appendChild(document.createElement('br'));
+		navModules.appendChild(document.createElement('br'));
 		for (var c = 0; c < list[course].c.length; c++) {
-			// Generate chapter label
+			// Generate chapter title
 			var chapter = list[course].c[c],
 			    chapterSpan = document.createElement('span');
 			chapterSpan.innerHTML = chapter.toUpperCase();
@@ -42,13 +70,15 @@ function initList() {
 				var sectionBtn = document.createElement('button');
 				sectionBtn.type = 'button';
 				sectionBtn.className = 'section';
-				sectionBtn.setAttribute('onclick', 'setFrame("'.concat(course, '","', chapter, '","', s, '")'));
+				sectionBtn.setAttribute('onclick', 'setFrame("'.concat(course, '","', chapter, '",', s, ')'));
 				sectionBtn.innerHTML = s;
 				navModules.appendChild(sectionBtn);
 			}
 			navModules.appendChild(document.createElement('br'));
 		}
 	}
+
+	setFrame('1a', '1a', 1);
 }
 
 function loadJSON(callback) {
@@ -62,12 +92,22 @@ function loadJSON(callback) {
 	xObj.send(null);
 }
 
-function toggleNav() {
+function toggleList() {
 	var navList = document.getElementById('nav-list');
 	if (navList.style.display === 'block')
 		navList.style.display = 'none';
 	else
 		navList.style.display = 'block';
+}
+
+function jumpPrev() {
+	// TODO: Jump to previous chapter
+	setFrame(currCourse, currChapter, currSection - 1);
+}
+
+function jumpNext() {
+	// TODO: Jump to next chapter
+	setFrame(currCourse, currChapter, currSection + 1);
 }
 
 function setCourse(course) {
@@ -78,4 +118,7 @@ function setFrame(course, chapter, section) {
 	var url = 'https://fgamedia.org/faculty/loceff/cs_courses/cs_'.concat(course, '/cs_', course.toUpperCase(), '_', chapter, '_', section, '.html');
 	document.getElementById('frame').src = url;
 	console.log(url);
+	currCourse = course;
+	currChapter = chapter;
+	currSection = section;
 }
